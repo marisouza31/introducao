924c97b7a4208e8ed791add4a46ee425dec3872f
marisouza31 <99839123+marisouza31@users.noreply.github.com>
2026-02-26 20:29:04 -0300

    implementando endpoints chamado Pessoa

--- src/main/java/io/github/fatec/introducao/controller/TestController.java
+++ src/main/java/io/github/fatec/introducao/controller/TestController.java
@@ -6,7 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.UUID;
 
 @RestController
-public class TestController {
+public class
+TestController {
 
     @GetMapping(value = "teste")
     public String teste() {
